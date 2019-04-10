baec642821a45672ccd9d0bdd973e4677fd569d7
SEC\user <dorothy_007@naver.com>
2019-04-11 01:32:38 +0900

    콜백함수 예시 삭제
    var myLogger 삭제

--- main.js
+++ main.js
@@ -4,13 +4,6 @@ var user = require('./routes/user');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
-var myLogger = function(req, res, next) {
-    console.log(req.url);
-    next();
-};
-
-app.use(myLogger);
-
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
